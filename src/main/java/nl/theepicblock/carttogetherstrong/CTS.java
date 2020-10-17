cd407a2deeef22bd0411b6b773733926205c500b
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-10-17 21:31:00 +0200

    init repo

---
+++ src/main/java/nl/theepicblock/carttogetherstrong/CTS.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.carttogetherstrong;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CTS implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
